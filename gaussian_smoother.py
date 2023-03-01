a43286f9f07c65665fccf9d41cf3e2132735a40a
Subhav Mittal <mittalsubhav@gmail.com>
2023-03-02 03:44:42 +0530

    Master restored to image_load, no smoothening

--- gaussian_smoother.py
+++
@@ -1,6 +0,0 @@
-# Function to smooth an image with a Gaussian filter
-from scipy import ndimage
-def gaussian_smoother(image, sigma=1):
-    """Smooth the image with a Gaussian filter"""
-    smooth_image = ndimage.gaussian_filter(image, sigma=sigma)
-    return smooth_image
